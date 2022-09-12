e61af94a427f8f34bdb2fddae2fdf94fc9e16a1f
少轻狂 <1677568218@qq.com>
2022-09-12 20:10:08 +0800

    feat(skl): sklSign

--- skl/sign.js
+++ skl/sign.js
@@ -2,22 +2,22 @@ const { sklLogin } = require("./login.js");
 const { getRandomString } = require("../utils/random.js");
 
 const payload = {
-	CurrentLocation: "浙江省杭州市钱塘区",
-	City:            "杭州市",
-	DistrictAdcode:  "330114",
-	Province:        "浙江省",
-	District:        "钱塘区",
-	HealthCode:      0,
-	HealthReport:    0,
-	CurrentLiving:   0,
-	Last14Days:      0,
+	"currentLocation": "浙江省杭州市钱塘区",
+	"healthCode": 0,
+	"city": "杭州市",
+	"districtAdcode": "330114",
+	"province": "浙江省",
+	"district": "钱塘区",
+	"healthReport": 0,
+	"currentLiving": 0,
+	"last14days": 0
 }
 
 async function sklSign(username,password) {
   const token = await sklLogin(username,password)
   const res = await fetch("https://skl.hdu.edu.cn/api/punch",{
     method: 'POST',
-    body: payload,
+    body: JSON.stringify(payload),
     headers: {
       'Content-Type': 'application/json',
       'x-auth-token': token,
